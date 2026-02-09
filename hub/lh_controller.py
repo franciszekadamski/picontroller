c1f0da8d807c51a6e085b709a9f653b8042ad117
Franciszek Adamski <102913434+franciszekadamski@users.noreply.github.com>
2026-02-09 19:51:28 +0100

    fix type bug

--- hub/lh_controller.py
+++ hub/lh_controller.py
@@ -15,23 +15,43 @@ class LHController(BasicController):
 
 
     def is_dry(self):
-        humidity_reading = self.send('humidity_sensor:read')
+        humidity_reading = self.send('humidity_sensor:read_humidity')
+        temperature_reading = self.send('humidity_sensor:read_temperature')
         moisture_reading = self.send('moisture_sensor:read')
 
-        humidity_raw = float(humidity_reading.split(':')[1])
-        moisture_raw = float(moisture_reading.split(':')[1])
+        try:
+            humidity_p = float(humidity_reading.split(':')[1])
+        except Exception as e:
+            print(e)
+            humidity_p = 300
 
-        humidity_a = self.zmq_data_store['humidity_lr_a_factor']
-        humidity_b = self.zmq_data_store['humidity_lr_b_factor']
+        try:
+            temperature_c = float(temperature_reading.split(':')[1])
+        except Exception as e:
+            print(e)
+            temperature_c = 300
+
+        try:
+            moisture_raw = float(moisture_reading.split(':')[1])
+        except Exception as e:
+            print(e)
+            moisture_raw = 300
+
         moisture_a = self.zmq_data_store['moisture_lr_a_factor']
         moisture_b = self.zmq_data_store['moisture_lr_b_factor']
 
-        humidity = humidity_a * humidity_raw + humidity_b
-        moisture = moisture_a * moisture_raw + moisture_b
+        moisture_p = moisture_a * moisture_raw + moisture_b
 
-        if humidity < self.zmq_data_store['humidity_watering_treshold_p']:
+        print(f"humidity: {humidity_p}")
+        print(f"temperature: {temperature_c}")
+        print(f"moisture: {moisture_p}")
+        self.zmq_data_store['humidity'] = humidity_p
+        self.zmq_data_store['temperature'] = temperature_c
+        self.zmq_data_store['moisture'] = moisture_p
+
+        if humidity_p < self.zmq_data_store['humidity_watering_treshold_p']:
             return True
-        elif moisture < self.zmq_data_store['moisture_watering_treshold_p']:
+        elif moisture_p < self.zmq_data_store['moisture_watering_treshold_p']:
             return True
         else:
             return False
@@ -47,24 +67,32 @@ class LHController(BasicController):
 
     def main(self):
         self.lighting_start_time = datetime.time(
-            self.zmq_data_store['lighting_start_time_h'],
-            self.zmq_data_store['lighting_start_time_min']
+            int(self.zmq_data_store['lighting_start_time_h']),
+            int(self.zmq_data_store['lighting_start_time_min'])
         )
         self.lighting_end_time = datetime.time(
-            self.zmq_data_store['lighting_end_time_h'],
-            self.zmq_data_store['lighting_end_time_min']
+            int(self.zmq_data_store['lighting_end_time_h']),
+            int(self.zmq_data_store['lighting_end_time_min'])
         )
         now = time.monotonic()
 
         if now - self.last_watering_check_time >= self.zmq_data_store['watering_interval_s']:
             self.last_watering_check_time = now
             if self.is_dry():
-                answer = self.send('water_servo:swing')
+                # answer = self.send('water_servo:swing')
+                self.send('water_servo:up')
+                time.sleep(0.5)
+                self.send('water_servo:down')
 
         if self.is_light_time():
-            answer = self.send('light_servo:up')
-        else:
             answer = self.send('light_servo:down')
+        else:
+            answer = self.send('light_servo:up')
+
+        if self.zmq_data_store['request']:
+            answer = self.send(self.zmq_data_store['request'])
+            self.zmq_data_store['request'] = ''
+            time.sleep(0.5)
 
         self.send('builtin_led:blink')
 
