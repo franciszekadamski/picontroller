8fbe8f3d3758c359f28ee0ddc2be2f2db2b758ab
Franciszek Adamski <adams.franciszek@wp.pl>
2026-03-11 23:11:00 +0100

    restructure project

--- hub/main.py
+++ hub/main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 
+import os
 import time
 
 from lh_controller import LHController
 
 
-CONFIGURATION_FILE_PATH = 'configuration_files/configuration.json'
+CONFIGURATION_FILE_PATH = os.environ['PICONTROLLER_CONFIGURATION_PATH']
 
 
 if __name__ == "__main__":
