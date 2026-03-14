47d32a9afadd449aebc78c4854b579e178e23c38
Franciszek Adamski <adams.franciszek@wp.pl>
2026-03-14 22:11:44 +0100

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 mkdir -p $HOME/.local/share/picontroller
+rm -rf $HOME/.local/share/picontroller
 cd $HOME/.local/share/
+
 git clone https://github.com/franciszekadamski/picontroller.git
 cd $HOME/.local/share/picontroller
 git checkout 18-prepare-installation-script
@@ -9,21 +11,43 @@ git checkout 18-prepare-installation-script
 export PICONTROLLER_PROJECT_PATH=$HOME/.local/share/picontroller
 export PATH=$PATH:$PICONTROLLER_PROJECT_PATH
 
-echo "export PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/.bashrc
-echo 'export PATH=$PATH:$PICONTROLLER_PROJECT_PATH' >> $HOME/.bashrc
+
+if ! grep -q "PICONTROLLER_PROJECT_PATH=" "$HOME/.bashrc"; then
+	  echo "export PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> "$HOME/.bashrc"
+fi
+
+if ! grep -q 'PATH=\$PATH:\$PICONTROLLER_PROJECT_PATH' "$HOME/.bashrc"; then
+	  echo 'export PATH=$PATH:$PICONTROLLER_PROJECT_PATH' >> "$HOME/.bashrc"
+fi
 
 source $HOME/.bashrc
 
-cp -r $PICONTROLLER_PROJECT_PATH/user_configuration $HOME/
-echo "PICONTROLLER_HUB_IP=127.0.0.1" >> $HOME/user_configuration/configuration.env
-echo "PICONTROLLER_HUB_USER=$USER" >> $HOME/user_configuration/configuration.env
-echo "PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/user_configuration/configuration.env
+if [ ! -d $HOME/user_configuration]; then
+    cp -r $PICONTROLLER_PROJECT_PATH/user_configuration $HOME/
+fi
+
+CONF_FILE="$HOME/user_configuration/configuration.env"
+
+grep -q "PICONTROLLER_HUB_IP=" "$CONF_FILE" || echo "PICONTROLLER_HUB_IP=127.0.0.1" >> "$CONF_FILE"
+
+grep -q "PICONTROLLER_HUB_USER=" "$CONF_FILE" || echo "PICONTROLLER_HUB_USER=$USER" >> "$CONF_FILE"
+
+grep -q "PICONTROLLER_PROJECT_PATH=" "$CONF_FILE" || echo "PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> "$CONF_FILE"
+
 
 export PICONTROLLER_CONFIGURATION_PATH=$HOME/user_configuration
 
+sudo systemctl stop picontroller.service
+sudo systemctl stop picontrollerserver.service
+sudo systemctl disable picontroller.service
+sudo systemctl disable picontrollerserver.service
+
+sudo rm /etc/systemd/system/picontroller.service
+sudo rm /etc/systemd/system/picontrollerserver.service
+
 picontroller_setup $PICONTROLLER_CONFIGURATION_PATH
 
-systemctl status picontroller.service
-systemctl status picontrollerserver.service
+echo $(systemctl status picontroller.service)
+echo $(systemctl status picontrollerserver.service)
 
 
