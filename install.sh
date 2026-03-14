8880634a53c99468ac4a772f8638c37795469d05
Franciszek Adamski <adams.franciszek@wp.pl>
2026-03-14 19:41:14 +0100

    fix

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ cd $HOME/.local/share/
 git clone https://github.com/franciszekadamski/picontroller.git
 cd $HOME/.local/share/picontroller
 git checkout 18-prepare-installation-script
-echo "export PICONTROLLER_PROJECT_PATH=$HOME/.local/share/picontroller" >> $HOME/.bashrc
-echo "export PATH=$PATH:$HOME/.local/share/picontroller" >> $HOME/.bashrc
+
+export PICONTROLLER_PROJECT_PATH=$HOME/.local/share/picontroller
+
+echo "export PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/.bashrc
+echo "export PATH=$PATH:$PICONTROLLER_PROJECT_PATH" >> $HOME/.bashrc
 
 source $HOME/.bashrc
 
@@ -14,6 +17,7 @@ cp -r $PICONTROLLER_PROJECT_PATH/user_configuration $HOME/user_configuration
 echo 'PICONTROLLER_HUB_IP="127.0.0.1"' >> $HOME/user_configuration/configuration.env
 echo "PICONTROLLER_HUB_USER=$USER" >> $HOME/user_configuration/configuration.env
 echo "PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/user_configuration/configuration.env
+
 export PICONTROLLER_CONFIGURATION_PATH=$HOME/user_configuration
 
 picontroller_setup $PICONTROLLER_CONFIGURATION_PATH
