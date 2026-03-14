e5d471db5ba07f980378319d7b5f5c52ce1050c7
Franciszek Adamski <adams.franciszek@wp.pl>
2026-03-14 21:39:39 +0100

    update

--- install.sh
+++ install.sh
@@ -15,9 +15,9 @@ echo 'export PATH=$PATH:$PICONTROLLER_PROJECT_PATH' >> $HOME/.bashrc
 source $HOME/.bashrc
 
 cp -r $PICONTROLLER_PROJECT_PATH/user_configuration $HOME/
-echo 'export PICONTROLLER_HUB_IP="127.0.0.1"' >> $HOME/user_configuration/configuration.env
-echo "export PICONTROLLER_HUB_USER=$USER" >> $HOME/user_configuration/configuration.env
-echo "export PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/user_configuration/configuration.env
+echo "PICONTROLLER_HUB_IP=127.0.0.1" >> $HOME/user_configuration/configuration.env
+echo "PICONTROLLER_HUB_USER=$USER" >> $HOME/user_configuration/configuration.env
+echo "PICONTROLLER_PROJECT_PATH=$PICONTROLLER_PROJECT_PATH" >> $HOME/user_configuration/configuration.env
 
 export PICONTROLLER_CONFIGURATION_PATH=$HOME/user_configuration
 
